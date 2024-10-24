ae2f62018664dd8ab1692a0f54cc493b4ede497b
gbibbo <gabobibbo@gmail.com>
2024-10-24 15:45:32 +0100

    Add requirements.txt and setup.py files

---
+++ setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sounds_of_home_analysis',
+    version='0.1.0',
+    author='Gabriel Bibbó',
+    author_email='g.bibbo@surrey.ac.uk',
+    description='Tools for visualizing sound events detected in the "Sounds of Home" experiment.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gbibbo/sounds_of_home_analysis',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'matplotlib>=3.0',
+        'numpy>=1.17',
+        'tqdm>=4.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
